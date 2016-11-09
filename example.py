f33845d29a229f5db5c8b9ff24431fdc39d00af3
Przemyslaw Bieganski <bieg4n@gmail.com>
2016-11-09 12:07:20 +0100

    Zaktualizowano example.py

--- example.py
+++ example.py
@@ -7,11 +7,18 @@ Created on Fri Sep 16 23:35:10 2016
 # import modulow
 import band
 import crawlerMain
+import geocoder
+
+# wartosci stale
+APIKEY = "" # klucz do google maps api - geocode
+SRCFILE = "./json/events.json" # lokalizacja events.json
+DESTPATH = "./sqlite" # gdzie zapisac dane wynikowe
+DJANGO_DB = "..\eventsMap\eventsMap" # sciezka do bazy danych SQLite aplikacji django [do wyswietlania danych]
 
 # Przypadek 1 - użycie klasy LastFmUser
 user= crawlerMain.LastFmUser('Biegan') # inicjuje obiekt
 user.get_scrobbles(3,3) # pobiera 3 stron - 150 ostatnio przesluchanych utworow
-user.get_artists(1,2) # pobiera jedna strone - 50 najczesciej sluchanych artystow
+user.get_artists(1,1) # pobiera jedna strone - 50 najczesciej sluchanych artystow
 user.get_concertList() # pobiera informacje o koncertach dla zespolow pobranych metoda get_artists
 user.to_json() # zrzuca wyniki do plikow JSON (lokalizacja /json)
 
@@ -19,7 +26,11 @@ user.to_json() # zrzuca wyniki do plikow JSON (lokalizacja /json)
 artist = band.BandFm('Sum+41', info = 1) # info == 1 jesli chcemy wiecej szczegolow na temat polaczenia
 artist.get_tags()
 artist.get_events()
-print "TAGI"
-print artist.tags
-print "Koncerty"
-print artist.events
+len(artist.tags)
+len(artist.events)
+
+# Przypadek 3 - uzycie geocoder
+dane = geocoder.Geocoder(APIKEY, SRCFILE, DESTPATH, DJANGO_DB)
+dane.process()
+dane.exportToJSON()
+dane.exportToDB()
